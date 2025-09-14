8f5054c0ead789bedd0d001e51c72cf36fd1725f
LaxmiNarayana31 <laxminarayana3101@gmail.com>
2025-09-14 18:33:01 +0530

    feat: implement GeminiLLM and EmbeddingGenerator classes for enhanced document and query embedding

--- app/helper/llm_helper.py
+++ app/helper/llm_helper.py
@@ -1,40 +1,89 @@
-import google.generativeai as genai
-from langchain.llms.base import LLM
-from langchain_core.embeddings import Embeddings
+import os
+import traceback
+
+from dotenv import load_dotenv
+from langchain_google_genai import chat_models
+from langchain_google_genai.chat_models import ChatGoogleGenerativeAI, ChatGoogleGenerativeAIError
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+from google.api_core.exceptions import ResourceExhausted, InvalidArgument, DeadlineExceeded
+
+from app.exceptions.gemini import GeminiException
 
 
-class GoogleGeminiEmbeddings(Embeddings):
-    def embed_documents(self, texts):
-        embeddings = []
-        for text in texts:
-            result = genai.embed_content(
-                model="models/text-embedding-004",
-                content=text,
-                task_type="retrieval_document",
+load_dotenv(verbose=True)
+google_api_key = os.getenv("GOOGLE_API_KEY")
+
+class GeminiLLM:
+    @staticmethod
+    def get_chat_llm_client():
+        try:
+            llm = ChatGoogleGenerativeAI(
+                model = "gemini-2.0-flash", 
+                google_api_key = google_api_key, 
+                temperature = 0.8, 
+                top_p = 0.85, 
+                max_retries = 0
             )
-            embeddings.append(result["embedding"])
-        return embeddings
+            # Override _chat_with_retry to bypass retries
+            chat_models._chat_with_retry = lambda generation_method, **kwargs: generation_method(**kwargs)
+            return llm
+        except Exception as e:
+            # Get the traceback as a string
+            traceback_str = traceback.format_exc()
+            print(traceback_str)
+            # Get the line number of the exception
+            line_no = traceback.extract_tb(e.__traceback__)[-1][1]
+            print(f"Exception occurred on line {line_no}")
+            return str(e)
+    
+class EmbeddingGenerator:
+    _model_name = "models/text-embedding-004"
+    _output_dim = 768
 
-    def embed_query(self, query):
-        result = genai.embed_content(
-            model="models/text-embedding-004",
-            content=query,
-            task_type="retrieval_query",
+    @staticmethod
+    def _get_embedding_model():
+        return GoogleGenerativeAIEmbeddings(
+            model=EmbeddingGenerator._model_name,
+            google_api_key=google_api_key
         )
-        return result["embedding"]
 
+    @staticmethod
+    def generate_document_embedding(text: str):
+        try:
+            document_embedding = EmbeddingGenerator._get_embedding_model().embed_documents(
+                texts=[text],
+                task_type="RETRIEVAL_DOCUMENT",
+                output_dimensionality=EmbeddingGenerator._output_dim
+            )
+            return document_embedding
+        except (ResourceExhausted, ChatGoogleGenerativeAIError, InvalidArgument, DeadlineExceeded) as e:
+            return GeminiException.handle_gemini_exception(e)
+        except Exception as e:
+            # Get the traceback as a string
+            traceback_str = traceback.format_exc()
+            print(traceback_str)
+            # Get the line number of the exception
+            line_no = traceback.extract_tb(e.__traceback__)[-1][1]
+            print(f"Exception occurred on line {line_no}")
+            return str(e)
+        
+    @staticmethod
+    def generate_query_embedding(text: str):
+        try:
+            query_embedding = EmbeddingGenerator._get_embedding_model().embed_query(
+                text=text,
+                task_type="RETRIEVAL_QUERY",
+                output_dimensionality=EmbeddingGenerator._output_dim
+            )
+            return query_embedding
+        except (ResourceExhausted, ChatGoogleGenerativeAIError, InvalidArgument, DeadlineExceeded) as e:
+            return GeminiException.handle_gemini_exception(e)
+        except Exception as e:
+            # Get the traceback as a string
+            traceback_str = traceback.format_exc()
+            print(traceback_str)
+            # Get the line number of the exception
+            line_no = traceback.extract_tb(e.__traceback__)[-1][1]
+            print(f"Exception occurred on line {line_no}")
+            return str(e)
 
-# Custom LLM class to get response from Google Gemini
-class GoogleGeminiLLM(LLM):
-    def _call(self, prompt, stop=None):
-        model = genai.GenerativeModel("gemini-1.5-flash")
-        response = model.generate_content(prompt).text
-        return response
-
-    @property
-    def _identifying_params(self):
-        return {"model_name": "Gemini 1.5 Flash"}
-
-    @property
-    def _llm_type(self):
-        return "gemini"
